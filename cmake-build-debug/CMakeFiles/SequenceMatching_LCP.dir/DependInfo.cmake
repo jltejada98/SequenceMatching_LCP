a87aaf3083905eebb957bcbe3af6a26067e362ee
jltejada98 <jltejada98@gmail.com>
2022-08-10 18:17:49 -0400

    Last commit before testing using regex to determine matches after determining whether match is present in all strings

--- cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/DependInfo.cmake
@@ -4,32 +4,15 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/Sais.c" "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/Sais.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "AppleClang")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/FileManipulation.cpp" "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/FileManipulation.cpp.o"
-  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/MatchLocations.cpp" "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/MatchLocations.cpp.o"
-  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/SequenceMatching.cpp" "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/SequenceMatching.cpp.o"
-  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/main.cpp" "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/Sais.c" "CMakeFiles/SequenceMatching_LCP.dir/Sais.c.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/Sais.c.o.d"
+  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/FileManipulation.cpp" "CMakeFiles/SequenceMatching_LCP.dir/FileManipulation.cpp.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/FileManipulation.cpp.o.d"
+  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/MatchLocations.cpp" "CMakeFiles/SequenceMatching_LCP.dir/MatchLocations.cpp.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/MatchLocations.cpp.o.d"
+  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/SequenceMatching.cpp" "CMakeFiles/SequenceMatching_LCP.dir/SequenceMatching.cpp.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/SequenceMatching.cpp.o.d"
+  "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/main.cpp" "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links.
--- cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/cmake_clean.cmake
@@ -1,9 +1,14 @@
 file(REMOVE_RECURSE
   "CMakeFiles/SequenceMatching_LCP.dir/FileManipulation.cpp.o"
+  "CMakeFiles/SequenceMatching_LCP.dir/FileManipulation.cpp.o.d"
   "CMakeFiles/SequenceMatching_LCP.dir/MatchLocations.cpp.o"
+  "CMakeFiles/SequenceMatching_LCP.dir/MatchLocations.cpp.o.d"
   "CMakeFiles/SequenceMatching_LCP.dir/Sais.c.o"
+  "CMakeFiles/SequenceMatching_LCP.dir/Sais.c.o.d"
   "CMakeFiles/SequenceMatching_LCP.dir/SequenceMatching.cpp.o"
+  "CMakeFiles/SequenceMatching_LCP.dir/SequenceMatching.cpp.o.d"
   "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o"
+  "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o.d"
   "SequenceMatching_LCP"
   "SequenceMatching_LCP.pdb"
 )
