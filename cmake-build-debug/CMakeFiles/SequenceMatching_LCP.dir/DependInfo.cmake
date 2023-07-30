813789278368e42d69bffe406e7167d9e79a487c
Jose Luis Tejada <joseluistejada@joses-mbp.mynetworksettings.com>
2023-07-30 15:11:53 -0400

    Last commit before attempting to store only one pair of LCP indecies but both sequence indecies

--- cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SequenceMatching_LCP.dir/DependInfo.cmake
@@ -15,8 +15,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/Users/joseluistejada/Documents/Other/Development/C++/SequenceMatching_LCP/main.cpp" "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o" "gcc" "CMakeFiles/SequenceMatching_LCP.dir/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
